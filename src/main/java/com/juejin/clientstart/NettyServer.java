2e2d284f2b0c0671a9e329ac70219d33a7cd0100
beer <beerdwq95@gmail.com>
2018-11-27 11:32:13 +0800

    netty服务端启动流程

---
+++ src/main/java/com/juejin/clientstart/NettyServer.java
@@ -0,0 +1,35 @@
+package com.juejin.clientstart;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/****************************************************
+ *
+ * @Description:   客户端启动流程分析
+ *
+ * @Author: DONGWENQI
+ * @Date: Created in 10:55 AM 2018/11/27
+ * @Modified By:
+ ****************************************************/
+public class NettyServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                    }
+                });
+
+        serverBootstrap.bind(8000);
+    }
+}
+
+
